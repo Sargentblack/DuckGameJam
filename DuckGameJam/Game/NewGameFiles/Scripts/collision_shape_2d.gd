bb92edba2c20f3e01ed74fe8c6eaebebfcbe9cbc
Sargentblack <167802501+Sargentblack@users.noreply.github.com>
2025-07-24 00:17:09 +0200

    File Declutter

--- DuckGameJam/Game/NewGameFiles/Scripts/collision_shape_2d.gd
+++
@@ -1,6 +0,0 @@
-extends CollisionShape2D
-
-func _on_player_body_entered(body):
-	if body.is_in_group("enemy"):
-		get_tree().reload_current_scene()
-		
--- DuckGameJam/scripts/player.gd
+++ DuckGameJam/scripts/player.gd
@@ -1,11 +1,22 @@
-extends Sprite2D
+extends CharacterBody2D
+
+@export var speed: float = 300.0
+@export var bullet_scene: PackedScene
+
+@onready var shoot_timer: Timer = $ShootTimer
+@onready var parallax_background := get_node("..") as ParallaxBackground  # Adjust path if needed
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _physics_process(delta: float) -> void:
+	_update_input()
+	_move_background(delta)
+	move_and_slide()
 
+func _update_input() -> void:
+	var input_direction: Vector2 = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = input_direction * speed
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _move_background(delta: float) -> void:
+	if parallax_background:
+		var motion: Vector2 = -velocity * delta
+		parallax_background.scroll_offset += motion
